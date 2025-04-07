e458a14cc73c0cdb9d82e7eddf7d367cf55910ed
imkunet <im@kunet.dev>
2025-04-06 23:49:24 -0400

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            bun
+          ];
+        };
+      });
+    };
+}
